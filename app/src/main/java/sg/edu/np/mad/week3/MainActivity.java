18ae7d0040fa0e86a1a16a68991d8d854b369c42
james <JamesYuRenxiang>
2020-05-20 10:29:11 +0800

    Week 3 Practical Done.

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
         MediaPlayer media = MediaPlayer.create(this, R.raw.baby_laughing);
         media.start();
+        media.stop();
         VideoView view = findViewById(R.id.videoView);
         view.setVideoURI(
                 Uri.parse("android.resource://" + getPackageName() + '/' + R.raw.rabbid) );
